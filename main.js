a216e9d5aa950a7df1268064540e05ec7e254604
kamaleldinmostafa87 <kamaleldin.mostafa87@gmail.com>
2024-05-04 16:39:28 +0300

    solvedandcomplete in vs code better

--- main.js
+++ main.js
@@ -3,40 +3,27 @@ import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+// document.querySelector('#app').innerHTML = `
+  
+// `;
 
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
 var isAnagram = function (s, t) {
   const lenghtOfT = t.length;
   const lengthOfS = s.length;
-  // for(let i = 0;i<lenghtOfT;i++ ){
-
-  // }
-  //   console.log(lenghtOfT,lengthOfS)
-
+  let newArrangedStr = ''
   // get each char from t and push it in an array
+  for(let i = 0;i<lengthOfS;i++){
+    for(let j = 0; j<lenghtOfT; j++){
+      if(t[i]==s[j]){
+        newArrangedStr +=t[j]
+      }
+    }
+
+  }
+  console.log("newa arranged str ",newArrangedStr)
+
 };
 
 // t = rac = >, s = car
-isAnagram('tar', 'car');
-setupCounter(document.querySelector('#counter'));
+console.log(isAnagram('tar', 'car'));
+console.log('kamal')
